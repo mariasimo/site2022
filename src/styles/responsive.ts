3d38438b15dd3af9f1c41f1519ccb66f384e13fa
María Simó <holasoymariasimo@gmail.com>
2021-12-25 19:07:34 +0100

    feat(styles): add showFromMixin at responsive utilities

--- src/styles/responsive.ts
+++ src/styles/responsive.ts
@@ -1,23 +1,35 @@
-const keysOf = <T extends Record<string, unknown>>(object: T): string[] =>
+import { css } from 'styled-components';
+
+const getKeysOf = <T extends Record<string, unknown>>(object: T): string[] =>
   Object.keys(object) as [];
 
-export const sizes: { [index: string]: number } = {
+export const sizes = {
   mobile: 480,
-  tablet: 767,
+  tabletPortrait: 767,
+  tabletLandscape: 960,
   laptop: 1200,
 };
 
 const minWidthQuery = (width: number) => `@media (min-width: ${width}px)`;
 
-export const from = keysOf(sizes).reduce(
-  (
-    acc: {
-      [index: string]: string;
-    },
-    key: keyof typeof sizes,
-  ): { [index: string]: string } => ({
+export type Breakpoint = keyof typeof sizes;
+
+export const from = getKeysOf(sizes).reduce(
+  (acc, key): { [index: string]: string } => ({
     ...acc,
-    [key]: minWidthQuery(sizes[key]),
+    [key]: minWidthQuery(sizes[key as Breakpoint]),
   }),
-  {},
+  {} as {
+    [index: string]: string;
+  },
 );
+
+export const showFromMixin = (breakpoint?: Breakpoint) =>
+  breakpoint
+    ? css`
+        display: none;
+        ${from[breakpoint]} {
+          display: flex;
+        }
+      `
+    : null;
