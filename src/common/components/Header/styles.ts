d3887116eb3a72080788c474c4c0fb2f72dc61ea
María Simó <holasoymariasimo@gmail.com>
2021-12-27 23:48:17 +0100

    refactor(components): refactor header styles to adapt to main grid

--- src/common/components/Header/styles.ts
+++ src/common/components/Header/styles.ts
@@ -2,24 +2,65 @@ import styled from 'styled-components';
 import { BodyS, BodySBold } from '$/styles/typography';
 import Dot from '$/assets/icons/dot.svg';
 import { Breakpoint, from, showFromMixin } from '$/styles/responsive';
+import DefaultBadge from '$/common/components/Badge';
 
 export const Container = styled.header`
   border-block-start: 1px solid ${({ theme }) => theme.colors.ink}75;
   padding-block: 0.75rem;
   display: grid;
-  grid-template-columns: 1fr auto;
-  gap: 1rem;
+  grid-template-columns: repeat(8, 1fr);
+  gap: 1.5rem;
 
   ${from.tabletPortrait} {
-    grid-template-columns: repeat(2, 1fr) auto;
-  }
-
-  ${from.tabletLandscape} {
-    grid-template-columns: repeat(3, 1fr) auto;
+    gap: 3rem;
   }
 `;
 
 export const Text = styled(BodyS).attrs({ as: 'p' })``;
+
+export const ColumnsContainer = styled.div`
+  display: flex;
+  gap: 1.5rem;
+  justify-content: end;
+  grid-column: 6/9;
+
+  ${from.mobile} {
+    justify-content: space-between;
+    grid-column: 4/9;
+  }
+
+  ${from.tabletPortrait} {
+    grid-column: 3/9;
+  }
+`;
+
+export const Row = styled.div`
+  gap: 1rem;
+  display: flex;
+  align-items: flex-start;
+  grid-column: 1/6;
+
+  ${from.mobile} {
+    grid-column: 1/4;
+  }
+
+  ${from.tabletPortrait} {
+    grid-column: 1/3;
+  }
+
+  ${Text} {
+    line-height: 1.2;
+  }
+`;
+
+export const Column = styled.div<{ $showFrom?: Breakpoint }>`
+  flex-direction: column;
+  ${({ $showFrom }) => showFromMixin($showFrom)}
+
+  ${Text} {
+    line-height: 1.4;
+  }
+`;
 
 export const Link = styled(BodyS)`
   text-decoration: none;
@@ -35,27 +76,14 @@ export const Bold = styled(BodySBold).attrs({ as: 'span' })`
   display: block;
 `;
 
-export const Row = styled.div`
-  gap: 1rem;
-  display: flex;
-  align-items: flex-start;
-
-  ${Text} {
-    line-height: 1.2;
-  }
-`;
-
-export const Column = styled.div<{ $showFrom?: Breakpoint }>`
-  flex-direction: column;
-
-  ${Text} {
-    line-height: 1.4;
-  }
-
-  ${({ $showFrom }) => showFromMixin($showFrom)}
-`;
-
 export const ThemeSwitcher = styled(Dot)`
   margin-block-start: 0.25rem;
+  flex-shrink: 0;
+`;
+
+export const Badge = styled(DefaultBadge)`
+  display: flex;
+  justify-self: flex-end;
+  white-space: nowrap;
   flex-shrink: 0;
 `;
