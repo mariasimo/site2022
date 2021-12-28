05819e6818e3c4555d72f4227a82956ad2d3f09e
María Simó <holasoymariasimo@gmail.com>
2021-12-28 19:01:52 +0100

    style(components): tweak ThemeSwitcher

--- src/common/components/ThemeSwitcher/styles.ts
+++ src/common/components/ThemeSwitcher/styles.ts
@@ -1,25 +1,26 @@
 import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
-  width: 1.5rem;
-  height: 1.5rem;
+  width: 1.2rem;
+  height: 1.2rem;
   border-radius: 50%;
   overflow: hidden;
   position: relative;
   transition: transform 300ms ease-in-out;
+  cursor: pointer;
 `;
 
 export const Circle = styled.div`
   background-color: ${({ theme }) => theme.colors.ink};
-  width: 1.5rem;
-  height: 1.5rem;
+  width: 100%;
+  height: 100%;
   border-radius: 50%;
 `;
 
 export const Mask = styled.div<{ $isLightTheme: boolean }>`
   transform-origin: 100% 100%;
-  width: 1.25rem;
-  height: 1.25rem;
+  width: 85%;
+  height: 85%;
   transition: transform 300ms ease-in-out;
   position: absolute;
   top: 0rem;
