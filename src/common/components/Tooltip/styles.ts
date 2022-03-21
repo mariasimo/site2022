cb4cdf2418c9ac94b273bbf732602d111025a020
María Simó <holasoymariasimo@gmail.com>
2022-03-21 23:46:43 +0100

    style(components): add pretty scrollbar

--- src/common/components/Tooltip/styles.ts
+++ src/common/components/Tooltip/styles.ts
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion';
 import styled from 'styled-components';
+import { prettyScrollbar } from '$/styles/mixins';
 
 export const Container = styled.span`
   position: relative;
@@ -23,4 +24,5 @@ export const Box = styled(motion.span)<{
   z-index: 1;
   overflow-y: auto;
   padding: 1.25rem;
+  ${prettyScrollbar()}
 `;
