679b6d4492888c98069186588014ce81f3c12e4e
María Simó <holasoymariasimo@gmail.com>
2023-03-18 19:39:03 +0100

    feat: add support for external links

--- src/common/components/MarkdownParser/index.tsx
+++ src/common/components/MarkdownParser/index.tsx
@@ -1,15 +1,30 @@
-import type { ReactNode } from 'react';
+import type { AnchorHTMLAttributes, ComponentType, ReactNode } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkUnwrapImages from 'remark-unwrap-images';
 import remarkGfm from 'remark-gfm';
+import type { ReactMarkdownProps } from 'react-markdown/lib/ast-to-react';
+
+const LinkComponent: ComponentType<
+  AnchorHTMLAttributes<HTMLAnchorElement> & ReactMarkdownProps
+> = ({ node, children, ...props }) => {
+  const linkProps = (node.properties?.href as string).includes('http')
+    ? { ...props, target: '_blank', rel: "noopener noreferrer'" }
+    : { ...props };
+  return <a {...linkProps}>{children}</a>;
+};
 
 export default function MarkdownParser({
   children,
-  components,
+  components: customComponents,
 }: {
   children: string;
   components?: { [tag: string]: ReactNode };
 }) {
+  const components = {
+    a: LinkComponent,
+    ...customComponents,
+  };
+
   return (
     <ReactMarkdown
       children={children}
