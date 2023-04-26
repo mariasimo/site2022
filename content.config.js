8e68d68993394aa41a84a7fde48c47849403369a
María Simó <holasoymariasimo@gmail.com>
2023-04-26 22:22:12 +0200

    fix: move data folder to root

--- content.config.js
+++ content.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   notesDirectory: 'content/notes/',
   learningInPublicFile: 'content/learning-in-public.md',
-  dataDirectory: 'public/data/notes/',
+  dataDirectory: 'data/notes/',
 };
