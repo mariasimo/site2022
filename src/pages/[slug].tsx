408e86c2d3bfcbd0846bafbcd9712044108982c3
María Simó <holasoymariasimo@gmail.com>
2023-07-27 09:13:06 +0200

    fix: fix static paths for notes

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,7 +1,12 @@
 import NoteView from '$/note';
-import type { GetStaticPropsContext, InferGetStaticPropsType } from 'next';
+import type {
+  GetStaticPathsContext,
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
+} from 'next';
 import { getNote, getRecommendedLinks, listNotes } from '../common/lib/notes';
 import { isLanguage } from '../common/lib/notes/types';
+import { getTranslationsList } from '../common/utils/getFiles';
 
 function NotePage({
   note,
@@ -11,19 +16,27 @@ function NotePage({
 
 export default NotePage;
 
-export async function getStaticPaths() {
-  const notes = await listNotes();
+export async function getStaticPaths({ locales }: GetStaticPathsContext) {
+  const notesPaths = await listNotes();
+  const notes = [
+    ...new Set(
+      notesPaths.map((path) => {
+        const [slug, locale] = path.split('/').filter(Boolean);
+        return { slug, locale };
+      }),
+    ),
+  ];
 
   return {
-    paths: notes.flatMap((note) => {
-      const [slug, locale] = note.split('/').filter(Boolean);
-
-      return {
-        params: {
-          slug,
-        },
-        locale,
-      };
+    paths: notes.flatMap(({ slug }) => {
+      return locales?.map((locale) => {
+        return {
+          params: {
+            slug,
+          },
+          locale,
+        };
+      });
     }),
     fallback: false,
   };
@@ -34,7 +47,11 @@ export async function getStaticProps({
   locale,
 }: GetStaticPropsContext) {
   const slug = params?.slug as string;
-  const note = getNote(slug, locale);
+  const translations = getTranslationsList(slug);
+  const checkedLocale =
+    locale && translations?.includes(locale) ? locale : translations[0];
+
+  const note = getNote(slug, checkedLocale);
 
   if (isLanguage(locale) && note) {
     const otherNotesLinks = await getRecommendedLinks(slug, locale);
